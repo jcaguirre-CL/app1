735083f75d44b48e3a0bd36f0717d551ccea5bf7
Juan Carlos A <jcaguirrecl@gmail.com>
2018-02-16 08:14:11 -0800

    creacion de 5 componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
+import { Component3Component } from './component3/component3.component';
+import { Component4Component } from './component4/component4.component';
+import { Component5Component } from './component5/component5.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component,
+    Component4Component,
+    Component5Component
   ],
   imports: [
     BrowserModule
